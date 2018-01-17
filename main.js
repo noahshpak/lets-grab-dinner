8ebf4e8797c6f41e2728212f1a2087d21e56693d
noahshpak <noah@shpakee.com>
2018-01-16 22:36:44 -0500

    basic search queries linked to Algolia index

--- main.js
+++ main.js
@@ -1,3 +1,26 @@
 $(document).ready(function() {
-  console.log("Hello from JS");
+  // Initialize the client
+  var client = algoliasearch("MOO6B8R0Z1", "a9b849cc21a51347b3082a5e30e70f4b");
+  var index = client.initIndex('open-table-restaurants');
+
+
+    // firstname
+  index.search('burger', function(err, content) {
+    console.log(content.hits);
+  });
+
+  // firstname with typo
+  index.search('steak', function(err, content) {
+    console.log(content.hits);
+  });
+
+  // a company
+  index.search('fine dining', function(err, content) {
+    console.log(content.hits);
+  });
+
+  // a firstname & company
+  index.search('4.5', function(err, content) {
+    console.log(content.hits);
+  });
 });
